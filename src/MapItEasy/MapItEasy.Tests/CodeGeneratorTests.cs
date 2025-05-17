f7e0b6e9fb48eed476dc48bf01311235c1d8d4cc
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-05-17 21:29:21 +0700

    Verify Tests

---
+++ src/MapItEasy/MapItEasy.Tests/CodeGeneratorTests.cs
@@ -0,0 +1,54 @@
+﻿namespace MapItEasy.Tests;
+
+public class CodeGeneratorTests
+{
+    [Fact]
+    public Task GenerateMappingCode_ReturnsExpectedCode()
+    {
+        // Act
+        var code = CodeGenerator.GenerateMappingCode<A, B>();
+
+        // Assert
+        return Verify(code);
+    }
+
+    [Fact]
+    public Task GenerateMappingCode_RespectsCustomVariableNames()
+    {
+        // Act
+        var code = CodeGenerator.GenerateMappingCode<A, B>("src", "dest");
+
+        // Assert
+        return Verify(code);
+    }
+
+    public class A
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Age { get; set; }
+
+        public int NotNullToNull { get; set; }
+
+        public int? NullToNotNull { get; set; }
+    }
+
+    public class B
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Age { get; set; }
+
+        public int? NotNullToNull { get; set; }
+
+        public int NullToNotNull { get; set; }
+    }
+}
